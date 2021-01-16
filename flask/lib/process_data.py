6d09a8557846c719a2abb758d6ea191a01d5dadb
kevin200617 <ky200617@gmail.com>
2021-01-16 11:27:45 -0500

    code from Build

--- flask/lib/process_data.py
+++ flask/lib/process_data.py
@@ -14,15 +14,42 @@ def find_disease_helper():
     return data
 
 # Converts the csv data into json format to make it easier to use.
-def convert_csv_to_json():
-    data = []
-    with open(f'{csvPath}/dataset.csv') as csvFile:
-        csvReader = csv.DictReader(csvFile)
-        for row in csvReader:
-            data.append(row)
 
+def convert_csv_to_json():
+    dict_data = []
+
+    current_dict = {}
+
+    previous_disease = 0
+    clock = 1
+
+    max_num_symptoms = 0
+
+    with open(f'{csvPath}/symptom_x_dataset.csv') as csvFile:
+        csvReader = csv.reader(csvFile)
+        next(csvReader)
+        
+        for row in csvReader:
+     
+            disease =  row[0] # Grab data
+            symptom = row[1]
+
+            if previous_disease == disease:
+                current_dict.update({'disease': disease})
+
+                current_dict.update({clock: symptom})
+                clock += 1
+            else:
+                if clock > max_num_symptoms:
+                    max_num_symptoms = clock
+
+                dict_data.append(current_dict)
+                clock = 0
+                current_dict = {}
+
+            previous_disease = disease
     with open(f'{jsonPath}/dataset.json', 'w') as jsonFile:
-        json.dump(data, jsonFile, indent=4)
+        json.dump(dict_data, jsonFile, indent=4)
 
 
 def autocomplete_symptoms():
