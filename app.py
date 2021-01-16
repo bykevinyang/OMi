fd31c23f3ec78352eadba313aa324837c67e9dda
Rami Janini <>
2021-01-16 08:28:23 +0200

    I added my name

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', user = 'OMI')
+    return render_template('index.html', user = 'Rami')
 
 
 if __name__ == '__main__':
