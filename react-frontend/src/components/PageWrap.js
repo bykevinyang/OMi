3ac26a86be44990cb22043b714f1a8b28f94405a
Adam <adam.isenberg4920@gmail.com>
2021-01-22 15:14:18 +0200

    fixed page padding problem

--- react-frontend/src/components/PageWrap.js
+++ react-frontend/src/components/PageWrap.js
@@ -7,17 +7,19 @@ const Wr1 = styled.div`
     height: 100vh;
     min-height: 700px;
     width: 100%;
+    max-width: 100%;
     min-width: 755px;
     background-image: linear-gradient(90deg, #DEA0B5 , #C2C2E3, #B3E3EA);
-    padding: 25px;
+    padding: 25px 0 25px 0;
 `;
 
 const Wr1adapt = styled.div`
     min-height: 100vh;
     width: 100%;
+    max-width: 100%;
     min-width: 755px;
     background-image: linear-gradient(90deg, #DEA0B5 , #C2C2E3, #B3E3EA);
-    padding: 25px;
+    padding: 25px 0 25px 0;
 `;
 
 const Wr = styled.div`
@@ -25,9 +27,10 @@ const Wr = styled.div`
     height: 100vh;
     min-height: 700px;
     width: 100%;
+    max-width: 100%;
     min-width: 755px;
     background: #0d1117;
-    padding: 25px;
+    padding: 25px 0 25px 0;
 `;
 
 class Page1 extends React.Component {
