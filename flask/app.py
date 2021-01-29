aba5422a3673da1a9f51db62b70e4283b18640b1
Rami Janini <>
2021-01-29 15:06:35 +0200

    Fixed Bugs.

--- flask/app.py
+++ flask/app.py
@@ -1,8 +1,7 @@
 import json
 import sqlite3
 from flask import Flask
-from flask import render_template, url_for
-
+from flask import render_template, url_for, jsonify, Response
 from lib.search_database import User, check_database, first_symptoms, disease_info
 
 app = Flask(__name__)
@@ -18,14 +17,18 @@ def fsymptoms(mode):
     with open('Data/json/dataset.json') as f:
         data = json.load(f)
     data = first_symptoms(data, mode)
-    return {'data' : data}
+    response = jsonify({'data' : data})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
 
 @app.route('/description/<disease_name>')
 def description(disease_name):
     data = disease_info(disease_name)
-    return {'data' : data}
+    response = jsonify({'data' : data})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
 
@@ -43,7 +46,9 @@ def symptoms(user_id, mode):
     data =  user.search_symptoms()
     conn.commit()
     conn.close()
-    return {'data' : data}
+    response = jsonify({'data' : data})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
 @app.route('/disease/<user_symptom>/u/<user_id>/')
@@ -60,7 +65,9 @@ def update_data(user_id, user_symptom):
     data =  user.search_disease()
     conn.commit()
     conn.close()
-    return {'data' : data}
+    response = jsonify({'data' : data})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
 if __name__ == '__main__':
